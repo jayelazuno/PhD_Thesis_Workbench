3167c13ad4ff9c70bc66a8189d4374bf9bc71de5
Joshua Ayelazuno <jayelazuno@gmail.com>
2024-09-10 00:56:10 -0500

    I started a galaxy tutorial

---
+++ scripts/galaxyRNA-seq-pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#############
+#author: Joshua Ayelazuno
+# This a RNA seq pipeline reproduced by on the command line by following a tutorial provide by galaxy
+# The tutorial can be accessed https://training.galaxyproject.org/training-material/topics/transcriptomics/tutorials/ref-based/tutorial.html
+# Generally, the reason we analyse RNA-seq data and other forms of seq data are:
+# What are the steps to process RNA-Seq data?
+
+How to identify differentially expressed genes across multiple experimental conditions?
+
+What are the biological functions impacted by the differential expression of genes?
